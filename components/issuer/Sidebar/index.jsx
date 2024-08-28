8b6532be208dde9917fc66e0b9ec5c9565a15c02
SoarinSkySagar <sagarrana.pvt@gmail.com>
2024-08-28 14:40:30 +0530

    boilerplate sidebar

---
+++ components/issuer/Sidebar/index.jsx
@@ -0,0 +1,32 @@
+"use client";
+
+import Head from "next/head";
+import Sidebar from "./Sidebar";
+import MenuBarMobile from "./MenuBarMobile";
+import { useState } from "react";
+
+export default function Layout({ pageTitle, children }) {
+  // Concatenate page title (if exists) to site title
+  let titleConcat = "Responsive Sidebar Example";
+  if (pageTitle) titleConcat = pageTitle + " | " + titleConcat;
+
+  // Mobile sidebar visibility state
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  return (
+    <>
+      <Head>
+        <title>{titleConcat}</title>
+      </Head>
+      <div className="min-h-screen">
+        <div className="flex">
+          <MenuBarMobile setter={setShowSidebar} />
+          <Sidebar show={showSidebar} setter={setShowSidebar} />
+          <div className="flex flex-col flex-grow w-screen md:w-full min-h-screen">
+            {children}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
